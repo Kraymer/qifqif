dabb58484d95f9f2d5e262e9661cea33228aa43c
Fabrice Laporte <kraymer@gmail.com>
2015-03-11 07:59:50 +0100

    don't overwrite rejected matchs lines

--- qifhack/__init__.py
+++ qifhack/__init__.py
@@ -13,52 +13,6 @@ from clint.textui import puts, prompt, colored
 from difflib import SequenceMatcher
 
 
-def read_single_keypress():
-    """Waits for a single keypress on stdin.
-
-    This is a silly function to call if you need to do it a lot because it has
-    to store stdin's current setup, setup stdin for reading single keystrokes
-    then read the single keystroke then revert stdin back after reading the
-    keystroke.
-
-    Returns the character of the key that was pressed (zero on
-    KeyboardInterrupt which can happen when a signal gets handled)
-
-    """
-    import termios, fcntl, sys, os
-    fd = sys.stdin.fileno()
-    # save old state
-    flags_save = fcntl.fcntl(fd, fcntl.F_GETFL)
-    attrs_save = termios.tcgetattr(fd)
-    # make raw - the way to do this comes from the termios(3) man page.
-    attrs = list(attrs_save) # copy the stored version to update
-    # iflag
-    attrs[0] &= ~(termios.IGNBRK | termios.BRKINT | termios.PARMRK
-                  | termios.ISTRIP | termios.INLCR | termios. IGNCR
-                  | termios.ICRNL | termios.IXON )
-    # oflag
-    attrs[1] &= ~termios.OPOST
-    # cflag
-    attrs[2] &= ~(termios.CSIZE | termios. PARENB)
-    attrs[2] |= termios.CS8
-    # lflag
-    attrs[3] &= ~(termios.ECHONL | termios.ECHO | termios.ICANON
-                  | termios.ISIG | termios.IEXTEN)
-    termios.tcsetattr(fd, termios.TCSANOW, attrs)
-    # turn off non-blocking
-    fcntl.fcntl(fd, fcntl.F_SETFL, flags_save & ~os.O_NONBLOCK)
-    # read a single keystroke
-    try:
-        ret = sys.stdin.read(1) # returns a single character
-    except KeyboardInterrupt:
-        ret = 0
-    finally:
-        # restore old state
-        termios.tcsetattr(fd, termios.TCSAFLUSH, attrs_save)
-        fcntl.fcntl(fd, fcntl.F_SETFL, flags_save)
-    return ret
-
-
 def prefilled_input(_prompt, prefill=''):
     readline.set_startup_hook(lambda: readline.insert_text(prefill))
     readline.redisplay()
@@ -124,9 +78,7 @@ def pick_category(payee, categories, default_choice=''):
             if match in payee:
                 break
             else:
-                puts(overwrite('Match...: %s' % diff(payee, match)))
-                read_single_keypress()
-                print '-> '+match
+                puts(overwrite('Match rejected...: %s') % diff(payee, match))
         categories[category].append(match)
     return (category, categories)
 
