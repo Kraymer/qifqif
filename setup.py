72b223d6a325de2c3730f4e232f0fc86027e985b
Fabrice Laporte <kraymer@gmail.com>
2015-06-10 23:46:17 +0200

    fix #17 by cleaning up README.rst from sphinx directives

--- setup.py
+++ setup.py
@@ -5,8 +5,63 @@
 
 import os
 import sys
+import re
 
 from setuptools import setup
+
+
+def yield_sphinx_only_markup(lines):
+    """http://stackoverflow.com/a/25900928/5181
+       Cleans-up Sphinx-only constructs (ie from README.rst),
+       so that *PyPi* can format it properly.
+       type `python setup.py --long-description` to generate the text
+    """
+    substs = [
+        # Selected Sphinx-only Roles.
+        #
+        (r':abbr:`([^`]+)`', r'\1'),
+        (r':ref:`([^`]+)`', r'`\1`_'),
+        (r':term:`([^`]+)`', r'**\1**'),
+        (r':dfn:`([^`]+)`', r'**\1**'),
+        (r':(samp|guilabel|menuselection):`([^`]+)`', r'``\2``'),
+
+
+        # Sphinx-only roles:
+        #        :foo:`bar`   --> foo(``bar``)
+        #        :a:foo:`bar` XXX afoo(``bar``)
+        #
+        # (r'(:(\w+))?:(\w+):`([^`]*)`', r'\2\3(``\4``)'),
+        (r':(\w+):`([^`]*)`', r'\1(``\2``)'),
+
+
+        # Sphinx-only Directives.
+        #
+        (r'\.\. doctest', r'code-block'),
+        (r'\.\. plot::', r'.. '),
+        (r'\.\. seealso', r'info'),
+        (r'\.\. glossary', r'rubric'),
+        (r'\.\. figure::', r'.. '),
+
+
+        # Other
+        (r'\|version\|', r'x.x.x'),
+    ]
+
+    regex_subs = [(re.compile(regex, re.IGNORECASE), sub)
+                  for (regex, sub) in substs]
+
+    def clean_line(line):
+        try:
+            for (regex, sub) in regex_subs:
+                line = regex.sub(sub, line)
+        except Exception as ex:
+            print("ERROR: %s, (line(%s)" % (regex, sub))
+            raise ex
+
+        return line
+
+    for line in lines:
+        yield clean_line(line)
 
 
 def publish():
@@ -17,33 +72,34 @@ if sys.argv[-1] == "publish":
     publish()
     sys.exit()
 
+readme_lines = open('README.rst').readlines()
 setup(name='qifqif',
-    version='0.3.0',
-    description='QIF file editing tool',
-    long_description=open('README.rst').read(),
-    author='Fabrice Laporte',
-    author_email='kraymer@gmail.com',
-    url='https://github.com/KraYmer/qifqif',
-    license='MIT',
-    platforms='ALL',
+      version='0.3.0',
+      description='QIF file editing tool',
+      long_description=''.join(yield_sphinx_only_markup(readme_lines)),
+      author='Fabrice Laporte',
+      author_email='kraymer@gmail.com',
+      url='https://github.com/KraYmer/qifqif',
+      license='MIT',
+      platforms='ALL',
 
-    packages=[
-      'qifqif',
-    ],
-
-    entry_points={
-      'console_scripts': [
-          'qifqif = qifqif:main',
+      packages=[
+        'qifqif',
       ],
-    },
 
-    install_requires=[
-        'clint',
-    ],
+      entry_points={
+        'console_scripts': [
+            'qifqif = qifqif:main',
+        ],
+      },
 
-    classifiers=[
-      'License :: OSI Approved :: MIT License',
-      'Programming Language :: Python',
-      'Environment :: Console',
-      'Topic :: Office/Business :: Financial :: Accounting'
-    ])
+      install_requires=[
+          'clint',
+      ],
+
+      classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Environment :: Console',
+        'Topic :: Office/Business :: Financial :: Accounting'
+      ])
