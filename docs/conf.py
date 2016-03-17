cf8503ac3d71f35cfe6dba4c065b22d3f9c257f6
Fabrice Laporte <kraymer@gmail.com>
2016-03-17 22:28:50 +0100

    attempt at fixing rtfd build badge

--- docs/conf.py
+++ docs/conf.py
@@ -123,7 +123,7 @@ html_theme_path = [alabaster.get_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = None
+# html_title = None
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 html_short_title = None
