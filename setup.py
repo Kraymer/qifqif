2b4173867cb829d8594e2e5dcfbb1d3e14b4a2f4
Fabrice Laporte <kraymer@gmail.com>
2017-07-25 21:18:28 +0200

    setup.py: always convert .md to .rst

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def coerce_file(fn):
         if mock.version.endswith('dev'):
             mock.version += str(int(time.time()))
         return mock
-    if 'upload' in sys.argv and fn.endswith('md'):  # convert markdown to rest
+    if fn.endswith('md'):  # convert markdown to rest
         text = '\n'.join([l for l in text.split('\n') if '[nopypi' not in l])
         text = re.sub(r':\S+:', '', text)  # no emojis
         with tempfile.NamedTemporaryFile(mode='w+') as tmp:
