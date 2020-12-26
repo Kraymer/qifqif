d92d8abe381249f4acb83162ed2154078a91d91d
Fabrice Laporte <fabrice@yescapa.com>
2020-12-26 12:11:46 +0100

    tests: fix test_completer expected result

--- test/test_ui.py
+++ test/test_ui.py
@@ -10,7 +10,7 @@ from qifqif import ui
 class TestUi(unittest.TestCase):
     def test_completer(self):
         self.assertEqual(
-            set(ui.complete_matches("A: B,C")), set(["A", "A: B,C", "B", "B,C", "C"])
+            set(ui.complete_matches("A: B,C")), set(['A:', 'B,C', 'A: B,C'])
         )
 
 
