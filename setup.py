cc0da61bfd98749a21f8cd485082c1a843c6767d
Fabrice Laporte <fabrice@yescapa.com>
2020-12-28 12:18:44 +0100

    chore: remove useless version magic alteration for "-dev" suffix

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@
 import codecs
 import os
 import re
-import time
 from setuptools import setup
 
 PKG_NAME = "qifqif"
@@ -31,7 +30,6 @@ with codecs.open("{}/__init__.py".format(PKG_NAME), encoding="utf-8") as fd:
     version = re.search(
         r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE
     ).group(1)
-    version = version.replace("dev", str(int(time.time())))
 
 setup(
     name=PKG_NAME,
